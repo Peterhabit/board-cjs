ce198b066a335af7614af24b788e3c8bd90da9a3
Peterhabit <jiseokchoij@gmail.com>
2023-04-19 17:08:17 +0900

    feat: 로그인 회원가입 작업

---
+++ src/main/java/idusw/springboot/boradthymleaf/controller/MemberController.java
@@ -0,0 +1,73 @@
+package idusw.springboot.boradthymleaf.controller;
+
+import idusw.springboot.boradthymleaf.domain.Member;
+import idusw.springboot.boradthymleaf.service.MemberService;
+import idusw.springboot.boradthymleaf.service.MemoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/members")
+public class MemberController {
+    //DI - Dependency Injection
+    MemberService memberService;
+    public MemberController(MemoService memoService) {
+        this.memberService = memberService;
+
+    }
+
+    @GetMapping("/login")
+    public String getLoginform(){
+        return "/members/login";
+    }
+
+     @PostMapping("/login")
+    public String PostLogin() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterForm(Model model){
+        model.addAttribute("member", Member.builder().build());
+        return "/members/register";
+    }
+
+    @PostMapping("/register")
+    public String createMember(@ModelAttribute("member") Member member, Model model){
+        System.out.println("this");
+        if(memberService.create(member) >0) // 정상적으로 레코드가 추가되면 추가된 레코드 수 반환
+            return "redirect:/";
+        else
+            return "main/error";
+    }
+
+    @GetMapping("/update")
+    public String getUpdateform(){
+        return "/members/update";
+    }
+
+    @PostMapping("/update")
+    public String updateMember(){
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteMember(){
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/forgot")
+    public String getForgotform(){
+        return "/members/forgot-password";
+    }
+
+    @PostMapping("/forgot")
+    public String forgotMemberPassword(){
+            return "redirect:/";
+        }
+
+
+
+}
---
+++ src/main/java/idusw/springboot/boradthymleaf/service/MemberServiceImpl.java
@@ -0,0 +1,76 @@
+package idusw.springboot.boradthymleaf.service;
+
+import idusw.springboot.boradthymleaf.domain.Member;
+import idusw.springboot.boradthymleaf.entity.MemberEntity;
+import idusw.springboot.boradthymleaf.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+    //DI - Ioc : 제어의 역전
+    MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public int create(Member m) {
+        MemberEntity entity = MemberEntity.builder()
+                .seq(m.getSeq())
+                .email(m.getEmail())
+                .name(m.getName())
+                .pw(m.getPw())
+                .build();
+        if(memberRepository.save(entity)!= null) // 저장성공
+            return 1;
+        else
+            return 0;
+    }
+
+    @Override
+    public Member read(Member m) {
+        return null;
+    }
+
+    @Override
+    public List<Member> readList() {
+        return null;
+    }
+
+    @Override
+    public int update(Member m) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Member m) {
+        return 0;
+    }
+}
